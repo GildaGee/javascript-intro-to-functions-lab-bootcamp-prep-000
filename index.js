5fdd96e88153032d025d667e19217a8a04d09a81
Gilda Snow <gilda.snow@gmail.com>
2019-05-28 05:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) 
     return "I can't hear you!";
 }
-  else if (string === string.toUpperCase()) 
+  if (string === string.toUpperCase()) 
     return "YES INDEED!";
-} else if ('I love you, Grandma.') {
+} if ('I love you, Grandma.') {
   return 'I love you, too.'
 }
